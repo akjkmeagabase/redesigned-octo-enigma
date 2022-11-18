864e649f9bf86d76ba5dcf3a69497715afb8f682
Dashon Gunnings <93340031+akjkmeagabase@users.noreply.github.com>
2022-11-18 09:21:03 -0500

    Initialize fix/local-dev-esm-modules

---
+++ packages/api/src/index.js
@@ -0,0 +1,116 @@
+/* eslint-env serviceworker */
+import { Router } from 'itty-router'
+import { errorHandler } from './error-handler.js'
+import { addCorsHeaders, withCorsHeaders, corsOptions } from './cors.js'
+import { withApiOrMagicToken, withMagicToken, withPinningAuthorized } from './auth.js'
+import { envAll } from './env.js'
+import { statusGet } from './status.js'
+import { carHead, carGet, carPut, carPost } from './car.js'
+import { uploadPost } from './upload.js'
+import { userLoginPost, userTokensPost, userTokensGet, userTokensDelete, userUploadsGet, userUploadsDelete, userAccountGet, userUploadsRename, userInfoGet } from './user.js'
+import { pinDelete, pinGet, pinPost, pinsGet } from './pins.js'
+import { metricsGet } from './metrics.js'
+import { versionGet } from './version.js'
+import {
+  withMode,
+  READ_ONLY,
+  READ_WRITE
+} from './maintenance.js'
+import { notFound } from './utils/json-response.js'
+import { nameGet, namePost } from './name.js'
+
+const router = Router()
+router.options('*', corsOptions)
+router.all('*', envAll)
+
+const auth = {
+  '🤲': handler => withCorsHeaders(handler),
+  '🔒': handler => withCorsHeaders(withApiOrMagicToken(handler)),
+  '👮': handler => withCorsHeaders(withMagicToken(handler)),
+  '📌': handler => auth['🔒'](withPinningAuthorized(handler))
+}
+
+const mode = {
+  '👀': handler => withMode(handler, READ_ONLY),
+  '📝': handler => withMode(handler, READ_WRITE)
+}
+
+/* eslint-disable no-multi-spaces */
+router.post('/user/login',          mode['👀'](auth['🤲'](userLoginPost)))
+router.get('/status/:cid',          mode['👀'](auth['🤲'](statusGet)))
+router.get('/car/:cid',             mode['👀'](auth['🤲'](carGet)))
+router.head('/car/:cid',            mode['👀'](auth['🤲'](carHead)))
+
+router.post('/car',                 mode['📝'](auth['🔒'](carPost)))
+router.put('/car/:cid',             mode['📝'](auth['🔒'](carPut)))
+router.post('/upload',              mode['📝'](auth['🔒'](uploadPost)))
+router.get('/user/uploads',         mode['👀'](auth['🔒'](userUploadsGet)))
+
+router.post('/pins',                mode['📝'](auth['📌'](pinPost)))
+router.post('/pins/:requestId',     mode['📝'](auth['📌'](pinPost)))
+router.get('/pins/:requestId',      mode['👀'](auth['📌'](pinGet)))
+router.get('/pins',                 mode['👀'](auth['📌'](pinsGet)))
+router.delete('/pins/:requestId',   mode['📝'](auth['📌'](pinDelete)))
+
+router.get('/name/:key',            mode['👀'](auth['🤲'](nameGet)))
+router.post('/name/:key',           mode['📝'](auth['🔒'](namePost)))
+
+router.delete('/user/uploads/:cid',      mode['📝'](auth['👮'](userUploadsDelete)))
+router.post('/user/uploads/:cid/rename', mode['📝'](auth['👮'](userUploadsRename)))
+router.get('/user/tokens',               mode['👀'](auth['👮'](userTokensGet)))
+router.post('/user/tokens',              mode['📝'](auth['👮'](userTokensPost)))
+router.delete('/user/tokens/:id',        mode['📝'](auth['👮'](userTokensDelete)))
+router.get('/user/account',              mode['👀'](auth['👮'](userAccountGet)))
+router.get('/user/info',                 mode['👀'](auth['👮'](userInfoGet)))
+/* eslint-enable no-multi-spaces */
+
+// Monitoring
+router.get('/metrics', mode['👀'](withCorsHeaders(metricsGet)))
+
+// Version
+router.get('/version', withCorsHeaders(versionGet))
+
+router.get('/', () => {
+  return new Response(
+    `
+<body style="font-family: -apple-system, system-ui">
+  <h1>⁂</h1>
+  <p>try
+    <a href='/car/bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy'>
+      /car/bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy
+    </a>
+  </p>
+</body>`,
+    {
+      status: 200,
+      headers: {
+        'content-type': 'text/html; charset=UTF-8'
+      }
+    }
+  )
+})
+
+router.get('/error', () => { throw new Error('A deliberate error!') })
+router.all('*', withCorsHeaders(() => notFound()))
+
+/**
+ * @param {Error} error
+ * @param {Request} request
+ * @param {import('./env').Env} env
+ */
+function serverError (error, request, env) {
+  return addCorsHeaders(request, errorHandler(error, env))
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent
+/** @typedef {{ waitUntil(p: Promise): void }} Ctx */
+
+export default {
+  async fetch (request, env, ctx) {
+    try {
+      return await router.handle(request, env, ctx)
+    } catch (error) {
+      return serverError(error, request, env)
+    }
+  }
+}
---
+++ packages/db/test/pinning.spec.js
@@ -0,0 +1,511 @@
+/* eslint-env mocha, browser */
+import assert from 'assert'
+import { normalizeCid } from '../../api/src/utils/normalize-cid'
+import { DBClient } from '../index'
+import { createUpload, createUser, createUserAuthKey, token } from './utils.js'
+import { CID } from 'multiformats/cid'
+import { sha256 } from 'multiformats/hashes/sha2'
+import * as pb from '@ipld/dag-pb'
+/* global crypto */
+
+const pinRequestTable = 'psa_pin_request'
+
+/**
+ * @param {number} code
+ * @returns {Promise<string>}
+ */
+async function randomCid (code = pb.code) {
+  const bytes = crypto.getRandomValues(new Uint8Array(10))
+  const hash = await sha256.digest(bytes)
+  return CID.create(1, code, hash).toString()
+}
+
+/**
+ *
+ * @param {*} pinRequestOutput
+ * @param {object} opt
+ * @param {boolean} [opt.withContent]
+ */
+const assertCorrectPinRequestOutputTypes = (pinRequestOutput) => {
+  assert.ok(typeof pinRequestOutput._id === 'string', '_id should be a string')
+  assert.ok(typeof pinRequestOutput.sourceCid === 'string', 'sourceCid should be a string')
+  assert.ok(Array.isArray(pinRequestOutput.pins), 'pin should be an array')
+  assert.ok(Date.parse(pinRequestOutput.created), 'created should be valid date string')
+  assert.ok(Date.parse(pinRequestOutput.updated), 'updated should be valid date string')
+  assert.ok(typeof pinRequestOutput.contentCid === 'string', 'contentCid should be a string')
+}
+
+describe('Pin Request', () => {
+  /** @type {DBClient & {_client: import('@supabase/postgrest-js').PostgrestClient }} */
+  const client = (new DBClient({
+    endpoint: 'http://127.0.0.1:3000',
+    token
+  }))
+  let user
+  let authKey
+  /**
+   * @type {import('../db-client-types').PsaPinRequestUpsertInput}
+   */
+  let aPinRequestInput
+
+  /**
+   * @type {import('../db-client-types').PsaPinRequestUpsertOutput}
+   */
+  let aPinRequestOutput
+
+  const cids = [
+    'QmdA5WkDNALetBn4iFeSepHjdLGJdxPBwZyY47ir1bZGAK',
+    'QmNvTjdqEPjZVWCvRWsFJA1vK7TTw1g9JP6we1WBJTRADM'
+  ]
+
+  const meta = { key: 'value' }
+  const origins = ['origin1', 'origin2']
+
+  const normalizedCids = cids.map(cid => normalizeCid(cid))
+
+  const pins = [
+    {
+      status: 'Pinning',
+      location: {
+        peerId: '12D3KooWFe387JFDpgNEVCP5ARut7gRkX7YuJCXMStpkq714ziK6',
+        peerName: 'web3-storage-sv15',
+        region: 'region'
+      }
+    },
+    {
+      status: 'Pinning',
+      location: {
+        peerId: '12D3KooWFe387JFDpgNEVCP5ARut7gRkX7YuJCXMStpkq714ziK7',
+        peerName: 'web3-storage-sv16',
+        region: 'region'
+      }
+    }
+  ]
+
+  // Create user and auth key
+  before(async () => {
+    user = await createUser(client)
+    authKey = await createUserAuthKey(client, parseInt(user._id, 10))
+  })
+
+  // Guarantee no Pin requests exist and create the ones needed for our tests
+  before(async () => {
+    // Make sure we don't have pinRequest and content
+    await client._client.from(pinRequestTable).delete()
+    const { count: countR } = await client._client.from(pinRequestTable).select('id', {
+      count: 'exact'
+    })
+    assert.strictEqual(countR, 0, 'There are still requests in the db')
+
+    aPinRequestInput = {
+      sourceCid: cids[0],
+      contentCid: normalizedCids[0],
+      meta,
+      origins,
+      pins,
+      authKey
+    }
+
+    aPinRequestOutput = await client.createPsaPinRequest(aPinRequestInput)
+  })
+
+  describe('Create Pin', () => {
+    it('creates a Pin Request', async () => {
+      const savedPinRequest = await client.getPsaPinRequest(authKey, aPinRequestOutput._id)
+      assert.ok(savedPinRequest)
+      assert.strictEqual(savedPinRequest._id, aPinRequestOutput._id)
+    })
+
+    it('returns the pin request with the expected meta, origins, cids, and auth key', async () => {
+      assertCorrectPinRequestOutputTypes(aPinRequestOutput)
+      assert.deepStrictEqual(aPinRequestOutput.meta, meta, 'metadata is not the one provided')
+      assert.deepStrictEqual(aPinRequestOutput.origins, origins, 'origins is not the one provided')
+      assert.strictEqual(aPinRequestOutput.sourceCid, cids[0], 'sourceCid is not the one provided')
+      assert.strictEqual(aPinRequestOutput.authKey, authKey, 'auth key is not the one provided')
+      assert.strictEqual(aPinRequestOutput.contentCid, normalizedCids[0], 'contentCid is not the one provided')
+    })
+
+    it('creates content and pins', async () => {
+      const { count: countContent } = await client._client
+        .from('content')
+        .select('cid', {
+          count: 'exact'
+        })
+        .match({
+          cid: normalizedCids[0]
+        })
+      assert.strictEqual(countContent, 1)
+
+      const { count: countPins } = await client._client
+        .from('pin')
+        .select('id', {
+          count: 'exact'
+        })
+        .match({
+          content_cid: normalizedCids[0]
+        })
+      assert.strictEqual(countPins, pins.length)
+    })
+
+    it('returns the right pins', async () => {
+      // Only checking statuses for simplicity
+      const statuses = aPinRequestOutput.pins
+        .map((p) => p.status)
+      assert.deepStrictEqual(statuses, [pins[0].status, pins[1].status])
+    })
+  })
+
+  describe('Get Pin', () => {
+    let savedPinRequest
+
+    before(async () => {
+      savedPinRequest = await client.getPsaPinRequest(authKey, aPinRequestOutput._id)
+    })
+
+    it('gets a Pin Request, if it exists', async () => {
+      assert.ok(savedPinRequest)
+    })
+
+    it('returns the right object', async () => {
+      assertCorrectPinRequestOutputTypes(savedPinRequest)
+      assert.strictEqual(savedPinRequest.sourceCid, cids[0], 'sourceCid is not the one provided')
+      assert.strictEqual(savedPinRequest.authKey, authKey, 'auth key is not the one provided')
+      assert.strictEqual(savedPinRequest.contentCid, normalizedCids[0], 'contentCid is not the one provided')
+    })
+
+    it('returns the right pins', async () => {
+      // Only checking statuses for simplicity
+      const statuses = savedPinRequest.pins
+        .map((p) => p.status)
+      assert.deepStrictEqual(statuses, [pins[0].status, pins[1].status])
+    })
+
+    it('throws if does not exists', async () => {
+      assert.rejects(client.getPsaPinRequest(authKey, 1000))
+    })
+  })
+
+  describe('Get Pins', () => {
+    let userPinList
+    let authKeyPinList
+    let createdPinningRequests
+
+    before(async () => {
+      userPinList = await createUser(client)
+      authKeyPinList = await createUserAuthKey(client, parseInt(userPinList._id, 10))
+      let pins = [
+        {
+          status: 'Pinning',
+          location: {
+            peerId: '12D3KooWFe387JFDpgNEVCP5ARut7gRkX7YuJCXMStpkq714ziK6',
+            peerName: 'web3-storage-sv15',
+            region: 'region'
+          }
+        },
+        {
+          status: 'Pinning',
+          location: {
+            peerId: '12D3KooWFe387JFDpgNEVCP5ARut7gRkX7YuJCXMStpkq714ziK7',
+            peerName: 'web3-storage-sv16',
+            region: 'region'
+          }
+        }
+      ]
+
+      const cidWithContent1 = await randomCid()
+      const normalizeCidWithContent1 = normalizeCid(cidWithContent1)
+      await createUpload(
+        client,
+        parseInt(userPinList._id, 10),
+        parseInt(authKeyPinList, 10),
+        normalizeCidWithContent1,
+        { pins }
+      )
+
+      pins = [
+        {
+          status: 'PinError',
+          location: {
+            peerId: '12D3KooWFe387JFDpgNEVCP5ARut7gRkX7YuJCXMStpkq714ziK6',
+            peerName: 'web3-storage-sv15',
+            region: 'region'
+          }
+        }
+      ]
+      const cidWithContent2 = await randomCid()
+      const normalizeCidWithContent2 = normalizeCid(cidWithContent1)
+      await createUpload(
+        client,
+        parseInt(userPinList._id, 10),
+        parseInt(authKeyPinList, 10),
+        normalizeCidWithContent2,
+        { pins }
+      )
+
+      pins = [
+        {
+          status: 'Pinned',
+          location: {
+            peerId: '12D3KooWFe387JFDpgNEVCP5ARut7gRkX7YuJCXMStpkq714ziK6',
+            peerName: 'web3-storage-sv15',
+            region: 'region'
+          }
+        }
+      ]
+      const cidWithContent3 = await randomCid()
+      const normalizeCidWithContent3 = normalizeCid(cidWithContent2)
+      await createUpload(
+        client,
+        parseInt(userPinList._id, 10),
+        parseInt(authKeyPinList, 10),
+        normalizeCidWithContent3,
+        { pins }
+      )
+
+      const pinRequestsInputs = [
+        {
+          name: 'horse',
+          date: [2020, 0, 1],
+          sourceCid: cidWithContent1,
+          cid: normalizeCidWithContent1
+        }, {
+          name: 'capybara',
+          date: [2020, 1, 1],
+          sourceCid: cidWithContent2,
+          cid: normalizeCidWithContent2
+        },
+        {
+          name: 'Camel',
+          date: [2020, 2, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'Giant Panda Bear',
+          date: [2020, 3, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'giant Schnoodle',
+          date: [2020, 4, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'giant worm',
+          date: [2020, 5, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'Zonkey Schnoodle',
+          date: [2020, 6, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'Zorse',
+          date: [2020, 7, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          date: [2020, 8, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: '',
+          date: [2020, 9, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'Bear',
+          date: [2020, 10, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'pin 1',
+          date: [2020, 10, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'pin 2',
+          date: [2020, 10, 1],
+          sourceCid: cidWithContent3,
+          cid: normalizeCidWithContent3
+        },
+        {
+          name: 'toBeDeleted',
+          date: [2020, 10, 1]
+        }
+      ]
+
+      createdPinningRequests = await Promise.all(pinRequestsInputs.map(async (item) => {
+        const sourceCid = item.sourceCid || await randomCid()
+        const normalizedCid = item.cid || normalizeCid(sourceCid)
+
+        return client.createPsaPinRequest({
+          ...(item.name) && { name: item.name },
+          authKey: authKeyPinList,
+          sourceCid: sourceCid,
+          contentCid: normalizedCid,
+          pins
+        })
+      }))
+    })
+
+    it('limits the results to 10', async () => {
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList)
+      assert.strictEqual(prs.length, 10)
+    })
+
+    it('limits the results to the provided limit', async () => {
+      const limit = 8
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList, {
+        limit
+      })
+      assert.strictEqual(prs.length, limit)
+    })
+
+    it('returns only requests for the provided token', async () => {
+      const { results: prs } = await client.listPsaPinRequests('10')
+      assert.strictEqual(prs.length, 0)
+    })
+
+    it('sorts by date', async () => {
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList)
+
+      const sorted = prs.reduce((n, item) => n !== null && item.created <= n.created && item)
+      assert(sorted)
+    })
+
+    it('filters items by provided status', async () => {
+      const { results: pins } = await client.listPsaPinRequests(authKeyPinList, {
+        statuses: ['Pinning', 'PinError']
+      })
+
+      assert.strictEqual(pins.length, 2)
+      assert(pins.map(p => p.name).includes('horse'))
+      assert(pins.map(p => p.name).includes('capybara'))
+    })
+
+    it('filters items by provided cid', async () => {
+      const cids = [createdPinningRequests[0].sourceCid, createdPinningRequests[1].sourceCid]
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList, {
+        cid: cids
+      })
+
+      assert.strictEqual(prs.length, 2)
+      assert(prs.map(p => p.sourceCid).includes(cids[0]))
+      assert(prs.map(p => p.sourceCid).includes(cids[1]))
+    })
+
+    it('filters items by exact match by default', async () => {
+      const name = 'capybara'
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList, {
+        name
+      })
+
+      assert.strictEqual(prs.length, 1)
+      prs.forEach(pr => {
+        assert.strictEqual(pr.name, name)
+      })
+    })
+
+    it('filters items by iexact match', async () => {
+      const name = 'camel'
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList, {
+        name,
+        match: 'iexact'
+      })
+
+      assert.strictEqual(prs.length, 1)
+      prs.forEach(pr => {
+        assert.strictEqual(pr.name.toLowerCase(), name.toLowerCase())
+      })
+    })
+
+    it('filters items by partial match', async () => {
+      const name = 'giant'
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList, {
+        name,
+        match: 'partial'
+      })
+
+      assert.strictEqual(prs.length, 2)
+      prs.forEach(pr => {
+        assert(pr.name.includes(name))
+      })
+    })
+
+    it('filters items by ipartial match', async () => {
+      const name = 'giant'
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList, {
+        name,
+        match: 'ipartial'
+      })
+
+      assert.strictEqual(prs.length, 3)
+      prs.forEach(pr => {
+        assert(pr.name.toLowerCase().includes(name.toLowerCase()))
+      })
+    })
+
+    it('filters items created before a date', async () => {
+      const { results: pins } = await client.listPsaPinRequests(authKeyPinList, {
+        before: '2021-01-01T00:00:00.000000Z'
+      })
+
+      assert.strictEqual(pins.length, 0)
+    })
+
+    it('filters items created after a date', async () => {
+      const { results: pins } = await client.listPsaPinRequests(authKeyPinList, {
+        after: '2021-01-01T00:00:00.000000Z',
+        limit: 20
+      })
+
+      assert.strictEqual(pins.length, 14)
+    })
+
+    it('unlists deleted pins', async () => {
+      const { results: prs } = await client.listPsaPinRequests(authKeyPinList, { limit: 20 })
+      assert.strictEqual(prs.length, 14)
+      await client.deletePsaPinRequest(createdPinningRequests[10]._id, authKeyPinList)
+      const { results: res } = await client.listPsaPinRequests(authKeyPinList, { limit: 20 })
+      assert.strictEqual(res.length, 13)
+    })
+  })
+
+  describe('Delete Pin', () => {
+    it('throws if the request id does not exist', async () => {
+      assert.rejects(client.deletePsaPinRequest(1000, authKey))
+    })
+
+    it('throws if the auth key does not belong to the pin request', async () => {
+      assert.rejects(client.deletePsaPinRequest(aPinRequestOutput._id, 'fakeAuth'))
+    })
+
+    it('returns the id of the deleted pin request', async () => {
+      const aPinRequestOutputId = aPinRequestOutput._id
+      const pinRequest = await client.getPsaPinRequest(authKey, aPinRequestOutputId)
+      assert.ok(!pinRequest.deleted, 'is null')
+      const deletedPinRequest = await client.deletePsaPinRequest(aPinRequestOutputId, authKey)
+      assert.ok(deletedPinRequest)
+      assert.equal(deletedPinRequest._id, pinRequest._id)
+    })
+
+    it('does not select pin request after deletion', async () => {
+      assert.rejects(client.getPsaPinRequest(authKey, aPinRequestOutput._id))
+    })
+
+    it('cannot delete a pin request which is already deleted', async () => {
+      assert.rejects(client.deletePsaPinRequest(aPinRequestOutput._id, authKey))
+    })
+  })
+})
